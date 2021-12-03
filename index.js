aa2503aa7564d8eff946d20827e315cf9da89339
Hassaan Rajwani <hassaanqadri258@gmail.com>
2021-12-03 19:01:55 +0500

    All Modified

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var table = document.getElementById("table") 
+var table = document.getElementById("table")
 function app(){
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then(function(data){
@@ -50,11 +50,11 @@ var i;
                 .then(function(boddy){
                     console.log(boddy)
 
-                    // getting comments in modal wale function ko call krwaya he 
+                    // getting comments in modal wale function ko call krwaya he
                     getComments(boddy)
                 })
                 
-                // bootstrap se copy kia he 
+                // bootstrap se copy kia he
                 $(document).ready(function(){
                     $(".btn").click(function(){
                         $("#myModal").modal('show');
@@ -77,18 +77,14 @@ function getComments(boddy){
 }
 
 // Pagination Adding
-function loop2(){
+function pegi2(){
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then(function(data){
             return data.json()
     })
 
     .then(function(detail){
-        loop2i(detail)
-    })
-}
-function loop2i(detail){
-    table.innerText = ""
+        table.innerText = ""
     for(i = 10; i < 20; i++){
         var tr = document.createElement("tr")
         var td1 = document.createElement("td")
@@ -106,30 +102,27 @@ function loop2i(detail){
             td3.appendChild(body)
             tr.appendChild(td3)
         
-            var td4 = document.createElement("td")
-            var button1 = document.createElement("button")
-                button1.className = "btn btn-primary"
-                button1.innerText = "Comments"
-                button1.onclick = comment(i)
-                    td4.appendChild(button1)
-                    tr.appendChild(td4)
+        var td4 = document.createElement("td")
+        var button1 = document.createElement("button")
+            button1.className = "btn btn-primary"
+            button1.innerText = "Comments"
+            button1.onclick = comment(i)
+                td4.appendChild(button1)
+                tr.appendChild(td4)
         
         table.appendChild(tr)
     }
+    })
 }
 
-function loop3(){
+function pegi3(){
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then(function(data){
             return data.json()
     })
 
     .then(function(detail){
-        loop3i(detail)
-    })
-}
-function loop3i(detail){
-    table.innerText = ""
+        table.innerText = ""
     for(i = 20; i < 30; i++){
         var tr = document.createElement("tr")
         var td1 = document.createElement("td")
@@ -147,30 +140,27 @@ function loop3i(detail){
             td3.appendChild(body)
             tr.appendChild(td3)
         
-            var td4 = document.createElement("td")
-            var button1 = document.createElement("button")
-                button1.className = "btn btn-primary"
-                button1.innerText = "Comments"
-                button1.onclick = comment(i)
-                    td4.appendChild(button1)
-                    tr.appendChild(td4)
+        var td4 = document.createElement("td")
+        var button1 = document.createElement("button")
+            button1.className = "btn btn-primary"
+            button1.innerText = "Comments"
+            button1.onclick = comment(i)
+                td4.appendChild(button1)
+                tr.appendChild(td4)
         
         table.appendChild(tr)
     }
+    })
 }
 
-function loop4(){
+function pegi4(){
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then(function(data){
             return data.json()
     })
 
     .then(function(detail){
-        loop4i(detail)
-    })
-}
-function loop4i(detail){
-    table.innerText = ""
+        table.innerText = ""
     for(i = 30; i < 40; i++){
         var tr = document.createElement("tr")
         var td1 = document.createElement("td")
@@ -188,47 +178,44 @@ function loop4i(detail){
             td3.appendChild(body)
             tr.appendChild(td3)
         
-            var td4 = document.createElement("td")
-            var button1 = document.createElement("button")
-                button1.className = "btn btn-primary"
-                button1.innerText = "Comments"
-                button1.onclick = comment(i)
-                    td4.appendChild(button1)
-                    tr.appendChild(td4)
+        var td4 = document.createElement("td")
+        var button1 = document.createElement("button")
+            button1.className = "btn btn-primary"
+            button1.innerText = "Comments"
+            button1.onclick = comment(i)
+                td4.appendChild(button1)
+                tr.appendChild(td4)
         
         table.appendChild(tr)
     }
+    })
 }
 
-function loop5(){
+function pegi5(){
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then(function(data){
             return data.json()
     })
 
     .then(function(detail){
-        loop5i(detail)
-    })
-}
-function loop5i(detail){
-    table.innerText = ""
-    for(i = 40; i < 50; i++){
-        var tr = document.createElement("tr")
-        var td1 = document.createElement("td")
-        var id = document.createTextNode(detail[i].id)
-            td1.appendChild(id)
-            tr.appendChild(td1)
-        
-        var td2 = document.createElement("td")
-        var title = document.createTextNode(detail[i].title)
-            td2.appendChild(title)
-            tr.appendChild(td2)
-        
-        var td3 = document.createElement("td")
-        var body = document.createTextNode(detail[i].body)
-            td3.appendChild(body)
-            tr.appendChild(td3)
-        
+        table.innerText = ""
+        for(i = 40; i < 50; i++){
+            var tr = document.createElement("tr")
+            var td1 = document.createElement("td")
+            var id = document.createTextNode(detail[i].id)
+                td1.appendChild(id)
+                tr.appendChild(td1)
+            
+            var td2 = document.createElement("td")
+            var title = document.createTextNode(detail[i].title)
+                td2.appendChild(title)
+                tr.appendChild(td2)
+            
+            var td3 = document.createElement("td")
+            var body = document.createTextNode(detail[i].body)
+                td3.appendChild(body)
+                tr.appendChild(td3)
+            
             var td4 = document.createElement("td")
             var button1 = document.createElement("button")
                 button1.className = "btn btn-primary"
@@ -236,40 +223,38 @@ function loop5i(detail){
                 button1.onclick = comment(i)
                     td4.appendChild(button1)
                     tr.appendChild(td4)
-        
-        table.appendChild(tr)
-    }
+            
+            table.appendChild(tr)
+        }
+        })  
 }
 
-function loop6(){
+
+function pegi6(){
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then(function(data){
             return data.json()
     })
 
     .then(function(detail){
-        loop6i(detail)
-    })
-}
-function loop6i(detail){
-    table.innerText = ""
-    for(i = 50; i < 60; i++){
-        var tr = document.createElement("tr")
-        var td1 = document.createElement("td")
-        var id = document.createTextNode(detail[i].id)
-            td1.appendChild(id)
-            tr.appendChild(td1)
-        
-        var td2 = document.createElement("td")
-        var title = document.createTextNode(detail[i].title)
-            td2.appendChild(title)
-            tr.appendChild(td2)
-        
-        var td3 = document.createElement("td")
-        var body = document.createTextNode(detail[i].body)
-            td3.appendChild(body)
-            tr.appendChild(td3)
-        
+        table.innerText = ""
+        for(i = 50; i < 60; i++){
+            var tr = document.createElement("tr")
+            var td1 = document.createElement("td")
+            var id = document.createTextNode(detail[i].id)
+                td1.appendChild(id)
+                tr.appendChild(td1)
+            
+            var td2 = document.createElement("td")
+            var title = document.createTextNode(detail[i].title)
+                td2.appendChild(title)
+                tr.appendChild(td2)
+            
+            var td3 = document.createElement("td")
+            var body = document.createTextNode(detail[i].body)
+                td3.appendChild(body)
+                tr.appendChild(td3)
+            
             var td4 = document.createElement("td")
             var button1 = document.createElement("button")
                 button1.className = "btn btn-primary"
@@ -277,40 +262,37 @@ function loop6i(detail){
                 button1.onclick = comment(i)
                     td4.appendChild(button1)
                     tr.appendChild(td4)
-        
-        table.appendChild(tr)
-    }
+            
+            table.appendChild(tr)
+        }        
+    })
 }
 
-function loop7(){
+function pegi7(){
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then(function(data){
             return data.json()
     })
 
     .then(function(detail){
-        loop7i(detail)
-    })
-}
-function loop7i(detail){
-    table.innerText = ""
-    for(i = 60; i < 70; i++){
-        var tr = document.createElement("tr")
-        var td1 = document.createElement("td")
-        var id = document.createTextNode(detail[i].id)
-            td1.appendChild(id)
-            tr.appendChild(td1)
-        
-        var td2 = document.createElement("td")
-        var title = document.createTextNode(detail[i].title)
-            td2.appendChild(title)
-            tr.appendChild(td2)
-        
-        var td3 = document.createElement("td")
-        var body = document.createTextNode(detail[i].body)
-            td3.appendChild(body)
-            tr.appendChild(td3)
-        
+        table.innerText = ""
+        for(i = 60; i < 70; i++){
+            var tr = document.createElement("tr")
+            var td1 = document.createElement("td")
+            var id = document.createTextNode(detail[i].id)
+                td1.appendChild(id)
+                tr.appendChild(td1)
+            
+            var td2 = document.createElement("td")
+            var title = document.createTextNode(detail[i].title)
+                td2.appendChild(title)
+                tr.appendChild(td2)
+            
+            var td3 = document.createElement("td")
+            var body = document.createTextNode(detail[i].body)
+                td3.appendChild(body)
+                tr.appendChild(td3)
+            
             var td4 = document.createElement("td")
             var button1 = document.createElement("button")
                 button1.className = "btn btn-primary"
@@ -318,12 +300,13 @@ function loop7i(detail){
                 button1.onclick = comment(i)
                     td4.appendChild(button1)
                     tr.appendChild(td4)
-        
-        table.appendChild(tr)
-    }
+            
+            table.appendChild(tr)
+        }  
+    })
 }
 
-function loop8(){
+function pegi8(){
     table.innerText = ""
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then(function(data){
@@ -331,27 +314,23 @@ function loop8(){
     })
 
     .then(function(detail){
-        loop8i(detail)
-    })
-}
-function loop8i(detail){
-    for(i = 70; i < 80; i++){
-        var tr = document.createElement("tr")
-        var td1 = document.createElement("td")
-        var id = document.createTextNode(detail[i].id)
-            td1.appendChild(id)
-            tr.appendChild(td1)
-        
-        var td2 = document.createElement("td")
-        var title = document.createTextNode(detail[i].title)
-            td2.appendChild(title)
-            tr.appendChild(td2)
-        
-        var td3 = document.createElement("td")
-        var body = document.createTextNode(detail[i].body)
-            td3.appendChild(body)
-            tr.appendChild(td3)
-        
+        for(i = 70; i < 80; i++){
+            var tr = document.createElement("tr")
+            var td1 = document.createElement("td")
+            var id = document.createTextNode(detail[i].id)
+                td1.appendChild(id)
+                tr.appendChild(td1)
+            
+            var td2 = document.createElement("td")
+            var title = document.createTextNode(detail[i].title)
+                td2.appendChild(title)
+                tr.appendChild(td2)
+            
+            var td3 = document.createElement("td")
+            var body = document.createTextNode(detail[i].body)
+                td3.appendChild(body)
+                tr.appendChild(td3)
+            
             var td4 = document.createElement("td")
             var button1 = document.createElement("button")
                 button1.className = "btn btn-primary"
@@ -359,12 +338,13 @@ function loop8i(detail){
                 button1.onclick = comment(i)
                     td4.appendChild(button1)
                     tr.appendChild(td4)
-        
-        table.appendChild(tr)
-    }
+            
+            table.appendChild(tr)
+        }      
+    })
 }
 
-function loop9(){
+function pegi9(){
     table.innerText = ""
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then(function(data){
@@ -372,27 +352,23 @@ function loop9(){
     })
 
     .then(function(detail){
-        loop9i(detail)
-    })
-}
-function loop9i(detail){
-    for(i = 80; i < 90; i++){
-        var tr = document.createElement("tr")
-        var td1 = document.createElement("td")
-        var id = document.createTextNode(detail[i].id)
-            td1.appendChild(id)
-            tr.appendChild(td1)
-        
-        var td2 = document.createElement("td")
-        var title = document.createTextNode(detail[i].title)
-            td2.appendChild(title)
-            tr.appendChild(td2)
-        
-        var td3 = document.createElement("td")
-        var body = document.createTextNode(detail[i].body)
-            td3.appendChild(body)
-            tr.appendChild(td3)
-        
+        for(i = 80; i < 90; i++){
+            var tr = document.createElement("tr")
+            var td1 = document.createElement("td")
+            var id = document.createTextNode(detail[i].id)
+                td1.appendChild(id)
+                tr.appendChild(td1)
+            
+            var td2 = document.createElement("td")
+            var title = document.createTextNode(detail[i].title)
+                td2.appendChild(title)
+                tr.appendChild(td2)
+            
+            var td3 = document.createElement("td")
+            var body = document.createTextNode(detail[i].body)
+                td3.appendChild(body)
+                tr.appendChild(td3)
+            
             var td4 = document.createElement("td")
             var button1 = document.createElement("button")
                 button1.className = "btn btn-primary"
@@ -400,12 +376,13 @@ function loop9i(detail){
                 button1.onclick = comment(i)
                     td4.appendChild(button1)
                     tr.appendChild(td4)
-        
-        table.appendChild(tr)
-    }
+            
+            table.appendChild(tr)
+        }        
+    })
 }
 
-function loop10(){
+function pegi10(){
     table.innerText = ""
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then(function(data){
@@ -413,27 +390,23 @@ function loop10(){
     })
 
     .then(function(detail){
-        loop10i(detail)
-    })
-}
-function loop10i(detail){
-    for(i = 90; i < 100; i++){
-        var tr = document.createElement("tr")
-        var td1 = document.createElement("td")
-        var id = document.createTextNode(detail[i].id)
-            td1.appendChild(id)
-            tr.appendChild(td1)
-        
-        var td2 = document.createElement("td")
-        var title = document.createTextNode(detail[i].title)
-            td2.appendChild(title)
-            tr.appendChild(td2)
-        
-        var td3 = document.createElement("td")
-        var body = document.createTextNode(detail[i].body)
-            td3.appendChild(body)
-            tr.appendChild(td3)
-        
+        for(i = 90; i < 100; i++){
+            var tr = document.createElement("tr")
+            var td1 = document.createElement("td")
+            var id = document.createTextNode(detail[i].id)
+                td1.appendChild(id)
+                tr.appendChild(td1)
+            
+            var td2 = document.createElement("td")
+            var title = document.createTextNode(detail[i].title)
+                td2.appendChild(title)
+                tr.appendChild(td2)
+            
+            var td3 = document.createElement("td")
+            var body = document.createTextNode(detail[i].body)
+                td3.appendChild(body)
+                tr.appendChild(td3)
+            
             var td4 = document.createElement("td")
             var button1 = document.createElement("button")
                 button1.className = "btn btn-primary"
@@ -441,7 +414,8 @@ function loop10i(detail){
                 button1.onclick = comment(i)
                     td4.appendChild(button1)
                     tr.appendChild(td4)
-        
-        table.appendChild(tr)
-    }
+            
+            table.appendChild(tr)
+        }        
+    })
 }
